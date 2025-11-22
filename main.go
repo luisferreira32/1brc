5f5c2fef07119e59445c91f4b67e9489db6c0100
Luis Ferreira <luis.pmm.ferreira@gmail.com>
2025-11-22 17:13:47 +0000

    attempt with tries

--- main.go
+++ main.go
@@ -15,8 +15,10 @@ import (
 )
 
 const (
-	readBufferSize = 1_048_576
-	educatedJump   = 4 // {city-name; 2:+};[-]{0-9},{0-99}
+	readBufferSize        = 1_048_576
+	educatedJump          = 4 // {city-name; 1:+};[-]{0-9},{0-99}
+	maxStationNameSize    = 100
+	maxUniqueStationNames = 10_000
 )
 
 func panicHandler() {
@@ -67,45 +69,62 @@ type solutionItem struct {
 	acc   float64
 }
 
-func solveLine(line []byte, solution map[string]*solutionItem) error {
-	for i := 2; i < len(line); i++ {
-		if line[i] == ';' {
-			name := string(line[:i])
-
-			s, ok := solution[name]
-			if !ok {
-				s = &solutionItem{}
-				solution[name] = s
-			}
-
-			temp, err := strconv.ParseFloat(string(line[i+1:]), 32)
-			if err != nil {
-				return fmt.Errorf("on line: %s, got err: %w", line, err)
-			}
-
-			s.acc += temp
-			s.count += 1
-			if s.max < temp {
-				s.max = temp
-			}
-			if s.min > temp {
-				s.min = temp
-			}
-			return nil
-		}
-	}
-	return fmt.Errorf("unexpected line with a ; break: %s", line)
+type trieNode struct {
+	letter   byte
+	branches []*trieNode
+	solution *solutionItem
 }
 
-func parseReadBuffer(b []byte, solution map[string]*solutionItem) (int, error) {
+func solveLine(line []byte, head *trieNode) error {
+	node := head
 	i := 0
-	p := 0
+	for {
+		if line[i] == ';' || i >= len(line) {
+			break
+		}
+		branchIndex := slices.IndexFunc(node.branches, func(n *trieNode) bool { return n.letter == line[i] })
+		if branchIndex == -1 {
+			newBranch := &trieNode{letter: line[i], branches: make([]*trieNode, 0, 50)}
+			node.branches = append(node.branches, newBranch)
+			node = newBranch
+		} else {
+			node = node.branches[branchIndex]
+		}
+		i++
+	}
+
+	if node.solution == nil {
+		node.solution = &solutionItem{}
+	}
+
+	temp, err := strconv.ParseFloat(string(line[i+1:]), 32)
+	if err != nil {
+		return fmt.Errorf("on line: %s, got err: %w", line, err)
+	}
+
+	node.solution.acc += temp
+	node.solution.count += 1
+	if node.solution.max < temp {
+		node.solution.max = temp
+	}
+	if node.solution.min > temp {
+		node.solution.min = temp
+	}
+	return nil
+}
+
+func parseReadBuffer(b []byte, head *trieNode) (int, error) {
+	var (
+		err error
+		i   = 0
+		p   = 0
+	)
 	for {
 		if i >= len(b) {
 			break
 		}
 		if b[i] == '\n' {
-			err := solveLine(b[p:i], solution)
+			err = solveLine(b[p:i], head)
 			if err != nil {
 				return 0, err
 			}
@@ -118,6 +137,21 @@ func parseReadBuffer(b []byte, solution map[string]*solutionItem) (int, error) {
 	return len(b) - p, nil
 }
 
+func printStationData(b []byte, i int, node *trieNode) {
+	b[i] = node.letter
+	if node.solution != nil {
+		mean := math.Round(10*node.solution.acc/float64(node.solution.count)) / 10 // rounded to 1 decimal point
+		fmt.Printf("%s=%.1f/%.1f/%.1f\n", b[:i+1], node.solution.min, mean, node.solution.max)
+		return
+	}
+	slices.SortFunc(node.branches, func(a, b *trieNode) int {
+		return int(a.letter) - int(b.letter)
+	})
+	for _, branch := range node.branches {
+		printStationData(b, i+1, branch)
+	}
+}
+
 // Emit to stdout sorted alphabetically by station name, and the result values
 // per station in the format <min>/<mean>/<max>, rounded to one fractional digit.
 func solve1brc(filename string) error {
@@ -126,7 +160,7 @@ func solve1brc(filename string) error {
 		return err
 	}
 
-	solution := make(map[string]*solutionItem)
+	head := &trieNode{branches: make([]*trieNode, 0, 50)}
 	b := make([]byte, readBufferSize)
 	p := 0
 
@@ -141,7 +175,7 @@ func solve1brc(filename string) error {
 		}
 
 		pn := p + n
-		p, err = parseReadBuffer(b[:pn], solution)
+		p, err = parseReadBuffer(b[:pn], head)
 		if err != nil {
 			return err
 		}
@@ -151,15 +185,12 @@ func solve1brc(filename string) error {
 		}
 	}
 
-	keys := make([]string, 0, len(solution))
-	for k := range solution {
-		keys = append(keys, k)
-	}
-	slices.Sort(keys)
-	for _, k := range keys {
-		item := solution[k]
-		mean := math.Round(10*item.acc/float64(item.count)) / 10 // rounded to 1 decimal point
-		fmt.Printf("%s=%.1f/%.1f/%.1f\n", k, item.min, mean, item.max)
+	printBuffer := make([]byte, maxStationNameSize)
+	slices.SortFunc(head.branches, func(a, b *trieNode) int {
+		return int(a.letter) - int(b.letter)
+	})
+	for _, branch := range head.branches {
+		printStationData(printBuffer, 0, branch)
 	}
 	return nil
 }
